358b11c7a5afa03ba2c6552b7ce4a38dc4b403d0
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2012-10-11 22:09:24 +0200

    code for localized exceptions

---
+++ code/localized_exceptions/src/main/java/com/practicalunittesting/MyList.java
@@ -0,0 +1,15 @@
+package com.practicalunittesting;
+
+public class MyList<T> {
+
+    public MyList() {
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void add(T i) {
+    }
+
+    public T get(T i) {
+        return null;
+    }
+}
---
+++ code/localized_exceptions/src/test/java/com/practicalunittesting/MyListTest.java
@@ -0,0 +1,35 @@
+package com.practicalunittesting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyListTest {
+
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testMyList() {
+        MyList<Integer> list = new MyList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(3);
+        list.add(4);
+        assertTrue(4 == list.get(4));
+        assertTrue(2 == list.get(1));
+        assertTrue(3 == list.get(2));
+
+        list.get(6);
+    }
+
+
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testNegative() {
+        MyList<Integer> list = new MyList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(3);
+        list.add(4);
+        list.get(-1);
+    }
+}
