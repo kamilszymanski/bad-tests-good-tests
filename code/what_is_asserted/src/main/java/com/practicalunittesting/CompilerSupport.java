8cfd8435d402d736010d0b0961b7447e408683ae
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2012-11-06 21:43:08 +0100

    more code examples

---
+++ code/what_is_asserted/src/main/java/com/practicalunittesting/CompilerSupport.java
@@ -0,0 +1,10 @@
+package com.practicalunittesting;
+
+import java.io.File;
+import java.util.List;
+
+public class CompilerSupport {
+    public String[] compile(File[] fakes) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
