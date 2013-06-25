e706ecf08a11490c5c92340e5d36a850cdb04aff
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2013-06-25 20:32:04 +0200

    about book being free

--- generateHtml.sh
+++
@@ -1,8 +0,0 @@
-#generates HTML version of the book
-
-VER=$(date +"%Y%m%d_%H%M")
-
-echo ""
-echo "HTML"
-mkdir -p target
-asciidoc -a icons -d book -a toc2 -a toclevels=3 -n -o target/bad_tests_good_tests_tomek_kaczanowski_$VER.html book.txt
